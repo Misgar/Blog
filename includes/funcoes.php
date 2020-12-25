298d3aaff917a91951d6d953a9208ccf5c6c805a
Renato AO <renato.oliveira34@fatec.sp.gov.br>
2020-12-25 01:38:26 -0300

    Funcionalidade para verificação e sanitização de tags maliciosas, evitando injeções sql.

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -6,7 +6,7 @@ function limparDados (string $dado) : string
     # Tags que não serão removidas
     $tags = '<p><strong><i><u><ol><li><h1><h2><h3>';
 
-    
+    # <p> = &lt;p&gt;
     $retorno = htmlentities(strip_tags($dado, $tags));
 
     return $retorno;
