3294a88b2850b7692d660e1ae784c24570c3e6b2
Renato AO <renato.oliveira34@fatec.sp.gov.br>
2020-12-25 01:39:28 -0300

    Finalizado arquivo de controle de ações do usuário dentro do sistema. Toda e qualque ação será verificada com base nesse arquivo, que se utiliza das querys previamente programadas de forma dinamica.

--- core/usuario_repositorio.php
+++ core/usuario_repositorio.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Gerenciando as ações dentro do sistema utilizando os metodos
+ * de acesso ao banco previamente configurados.
+ */
+
 session_start();
 
 require_once '../includes/funcoes.php';
@@ -17,3 +22,121 @@ foreach($_GET as $indice => $dado)
 {
     $$indice = limparDados($dado);
 }
+
+switch ($acao) {
+    case 'insert':
+        $dados = [
+            'nome'  => $nome,
+            'email' => $email,
+            'senha' => crypt($senha)
+        ];
+
+        insere(
+            'usuario',
+            $dados
+        );
+
+        break;
+
+    case 'update':
+        $id = (int) $id;
+        $dados = [
+            'nome' => $nome,
+            'email' => $email,
+        ];
+
+        $criterio = [
+            ['id', '=', $id]
+        ];
+
+        atualiza(
+            'usuario',
+            $dados,
+            $criterio
+        );
+
+        break;
+
+    case 'login':
+        # Verificando email e se está ativo
+        $criterio = [
+            ['email', '=', $email],
+            ['AND', 'ativo', '=', 1]
+        ];
+
+        # Retornando busca do usuario no banco
+        $retorno = buscar(
+            'usuario',
+            ['id', 'nome', 'email', 'senha', 'adm'],
+            $criterio
+        );
+
+        if(count($retorno) > 0){
+            if (crypt($senha, $retorno[0]['senha']) == $retorno[0]['senha']){
+                $_SESSION['login']['usuario'] = $retorno[0]; # Armazenando os dados do usuario retornado
+
+                # Verifica se está logado e armazena a url
+                if (!empty($_SESSION['url_retorno'])) 
+                {
+                    # Se sim, redireciona para url
+                    header('Location: ' . $_SESSION['url_retorno']);
+                    $_SESSION['url_retorno'] = '';
+                    exit;
+                }
+            }
+        }
+
+        break;
+    case 'logout':
+        session_destroy();
+        break;
+    
+    # Atualizando o status do usuario no sistema (ativo/desativado)
+    case 'status':
+        $id = (int) $id;
+        $valor = (int) $valor;
+
+        $dados = [
+            'ativo' => $valor
+        ];
+
+        $criterio = [
+            ['id', '=', $id]
+        ];
+
+        atualiza(
+            'usuario',
+            $dados,
+            $criterio
+        );
+
+        header('Location: ../usuarios.php');
+        exit;
+        break;
+
+        # Promoção para administrador
+    case 'adm':
+        $id = (int) $id;
+        $valor = (int) $valor;
+
+        $dados = [
+            'adm' => $valor
+        ];
+
+        $criterio = [
+            ['id', '=', $id]
+        ];
+
+        atualiza(
+            'usuario',
+            $dados,
+            $criterio
+        );
+
+        header('Location: ../usuarios.php');
+        exit;
+        break;
+
+}
+
+header('Location: ../index.php');
