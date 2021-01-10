3d6fce921a88b8a6b838cd9230795ff6a7e53bec
Renato AO <renato.oliveira34@fatec.sp.gov.br>
2021-01-10 20:23:53 -0300

    Finalizado arquivo include de Buscas. Iniciado home page

---
+++ includes/buscar.php
@@ -0,0 +1,5 @@
+<form action="" class="form-inline my-2 my-lg-0" method="get" action=''>
+    <input type="text" class="form-control mr-sm-2" type="search" name='busca' placeholder="Busca" aria-label="Busca">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+
+</form>
--- includes/menu.php
+++ includes/menu.php
@@ -8,8 +8,9 @@
             <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
             <li class="nav-item"><a href="usuario_formulario.php" class="nav-link">Cadastre-se</a></li>
             <li class="nav-item"><a href="login_formulario.php" class="nav-link">Login</a></li>
-            <li class="nav-item"><a href="post_formulario.php" class="nav-link">Incluir Post</a></li>         
-            <?php if ((isset($_SESSION['login'])) && ($_SESSION['login']['usuario']['adm'] ===1)) : ?>
+            <li class="nav-item"><a href="post_formulario.php" class="nav-link">Incluir Post</a></li> 
+            <!-- Apenas o administador pode lidar com menu "Usuarios" -->        
+            <?php if ((isset($_SESSION['login'])) && ($_SESSION['login']['usuario']['adm'] === 1)) : ?>
                 <li class="nav-item"><a href="usuarios.php" class="nav-link">Usuários</a></li>
             <?php endif; ?>
         
